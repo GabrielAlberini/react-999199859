5e23e29957c5f8c3cac3b08139f4207ec2bf39a9
gabrielalberini <gabialberini733@gmail.com>
2025-06-13 19:56:25 -0300

    add: router

---
+++ clase-3-13-6-2025/src/views/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <p>Hola desde el panel de administrador</p>
+      <Footer />
+    </>
+  )
+}
+
+export default Dashboard
---
+++ clase-3-13-6-2025/src/views/Register.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+
+const Register = () => {
+  return (
+    <>
+      <Header />
+      <h1>Hola desde la vista registro</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default Register
