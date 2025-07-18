420585a44e37ae6d72c68fd133309cf62c195387
gabrielalberini <gabialberini733@gmail.com>
2025-07-18 19:57:22 -0300

    clase 7 - incorporación de auth

---
+++ clase-7-18-7-2025/src/config/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// colocar las credencias propias a cada uno
+const firebaseConfig = {
+  apiKey: "AIzaSyDwHR_Gcv9QCiHRtFP5mmDnRcPghtMrODQ",
+  authDomain: "productos-utn.firebaseapp.com",
+  projectId: "productos-utn",
+  storageBucket: "productos-utn.firebasestorage.app",
+  messagingSenderId: "692029493149",
+  appId: "1:692029493149:web:ce135f7072d2997be46974"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { db, auth }
---
+++ clase-7-18-7-2025/src/views/Login.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+import Layout from "../components/Layout/Layout"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../config/firebase"
+import "../styles/Login.css"
+import { useNavigate } from "react-router-dom"
+
+const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+  const [message, setMessage] = useState(null)
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError(null)
+    setMessage(null)
+
+    console.log(email, password)
+
+
+    if (!email || !password) {
+      setError("Debes completar los campos...")
+      return
+    }
+
+    // intentar guardar o registrar un usuario.
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+      setMessage("Usuario loguedo con éxito...")
+      setEmail("")
+      setPassword("")
+      setTimeout(() => {
+        setMessage("Redirigiendo al home...")
+      }, 2000)
+      setTimeout(() => {
+        navigate("/")
+      }, 3000)
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
+  return (
+    <>
+      <Layout>
+        <section id="login-section">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit} >
+            <label htmlFor="email">Correo electrónico:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={(e) =>
+                setEmail(e.target.value)}
+            />
+
+            <label htmlFor="password">Contraseña:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) =>
+                setPassword(e.target.value)
+              }
+            />
+
+            <button>Registrarme</button>
+          </form>
+          <h5 className="error-message">{error}</h5>
+          <h5 className="success-message">{message}</h5>
+        </section>
+      </Layout>
+    </>
+  )
+}
+
+export default Login
